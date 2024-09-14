01f6932dd9540187591a5fe66945f2ab689daaa0
Behnaz Mardanzadeh <b.mardanzadeh@charisma.tech>
2024-09-14 20:56:24 +0330

    Coin Change Recursive.

--- src/main/java/org/algocoding/Runner.java
+++ src/main/java/org/algocoding/Runner.java
@@ -4,15 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-
 public class Runner {
     private static final int[] coins = new int[]{2, 3, 5};
-    private static int[][] memoization = new int[1000][3];
 
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
-            Arrays.stream(memoization).forEach(amounts -> Arrays.fill(amounts, -1));
+
             int amount = Integer.valueOf(reader.readLine());
             int totalCombinations = coinChange(amount, 0);
             System.out.println(String.format("Total Combinations: %,d\n", totalCombinations));
@@ -31,11 +28,7 @@ public class Runner {
             return 0;
         }
 
-        if (memoization[amount][coinIndex] > -1) {
-            return memoization[amount][coinIndex];
-        }
-
-        return memoization[amount][coinIndex] = coinChange(amount - coins[coinIndex], coinIndex) + coinChange(amount, coinIndex + 1);
+        return coinChange(amount - coins[coinIndex], coinIndex) + coinChange(amount, coinIndex + 1);
     }
 }
 
